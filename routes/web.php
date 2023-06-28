8c25aedf77c2adccdb7c467083b83e8354760129
ay-yagi <ay-yagi@usen-next.jp>
2023-06-28 17:13:27 +0900

    yagi login commit

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 //コントローラー
-use App\Http\Controllers\CloneGramController;
-
-
-//コントローラー
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CloneGramController;
 
 
@@ -25,6 +22,19 @@ use App\Http\Controllers\CloneGramController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
 
 //チャット画面表示
