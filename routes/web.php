9619625e3ed9be0a747ea0eada0bd820edeb6173
ay-yagi <ay-yagi@usen-next.jp>
2023-06-29 13:52:55 +0900

    webにauthいれた

--- routes/web.php
+++ routes/web.php
@@ -36,44 +36,35 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
+Route::group(['middleware' => ['auth']], function(){
 
-//チャット画面表示
-Route::get('/Clone-gram/chat', [CloneGramController::class, 'chat']);
+    //チャット画面表示
+    Route::get('/Clone-gram/chat', [CloneGramController::class, 'chat']);
 
+    //ホーム画面（一覧画面）表示
+    Route::get('/Clone-gram/index', [CloneGramController::class, 'index'] );
 
+    //投稿ページ
+    Route::get('/Clone-gram/post_article', [CloneGramController::class, 'post_article'] );
 
-//ホーム画面（一覧画面）表示
-Route::get('/Clone-gram/index', [CloneGramController::class, 'index'] );
+    //投稿した記事の編集画面
+    Route::get('/Clone-gram/edit_article', [CloneGramController::class, 'edit_article'] );
 
+    //マイページ
+    Route::get('/Clone-gram/my_page', [CloneGramController::class, 'my_page'] );
 
- //投稿ページ
-Route::get('/Clone-gram/post_article', [CloneGramController::class, 'post_article'] );
+    //プロフィール編集画面
+    Route::get('/Clone-gram/edit_profile', [CloneGramController::class, 'edit_profile'] );
 
+    //プロフィール編集確認画面
+    Route::get('/Clone-gram/confirm_profile', [CloneGramController::class, 'confirm_profile'] );
 
-//投稿した記事の編集画面
-Route::get('/Clone-gram/edit_article', [CloneGramController::class, 'edit_article'] );
+    //他ユーザーのページ
+    Route::get('/Clone-gram/friends_page', [CloneGramController::class, 'friends_page'] );
 
+    //フォロワー表示画面
+    Route::get('/Clone-gram/follower', [CloneGramController::class, 'follower'] );
 
-//マイページ
-Route::get('/Clone-gram/my_page', [CloneGramController::class, 'my_page'] );
-
-
-//プロフィール編集画面
-Route::get('/Clone-gram/edit_profile', [CloneGramController::class, 'edit_profile'] );
-
-
-//プロフィール編集確認画面
-Route::get('/Clone-gram/confirm_profile', [CloneGramController::class, 'confirm_profile'] );
-
-
-//他ユーザーのページ
-Route::get('/Clone-gram/friends_page', [CloneGramController::class, 'friends_page'] );
-
-
-//フォロワー表示画面
-Route::get('/Clone-gram/follower', [CloneGramController::class, 'follower'] );
-
-
-//フォロ中のユーザーの表示画面
-Route::get('/Clone-gram/following', [CloneGramController::class, 'following'] );
-
+    //フォロ中のユーザーの表示画面
+    Route::get('/Clone-gram/following', [CloneGramController::class, 'following'] );
+});
