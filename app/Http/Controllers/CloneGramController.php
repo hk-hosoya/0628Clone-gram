a2b656b8278a0d508842f904ffae7a58796434cb
USENGROUP\hk-hosoya <hk-hosoya@usen-next.jp>
2023-06-29 14:16:58 +0900

    hosoya

--- app/Http/Controllers/CloneGramController.php
+++ app/Http/Controllers/CloneGramController.php
@@ -7,73 +7,107 @@ use Illuminate\Http\Request;
 class CloneGramController extends Controller
 {
     //ホーム画面（一覧画面）表示
-    public function index(){
+    public function index()
+    {
         return view('Clone-gram.index');
     }
 
     //投稿ページ
-    public function post_article(){
+    public function post_article()
+    {
         return view('Clone-gram.post_article');
     }
 
     //投稿した記事の編集画面
-    public function edit_article(){
+    public function edit_article()
+    {
         return view('Clone-gram.edit_article');
     }
 
     //アカウント追加画面表示
-    public function add_user(){
+    public function add_user()
+    {
         return view('Clone-gram.add_user');
     }
 
     //アカウント登録確認画面
-    public function confirm_user(){
+    public function confirm_user()
+    {
         return view('Clone-gram.confirm_user');
     }
 
     //アカウント登録完了画面
-    public function complete_user(){
+    public function complete_user()
+    {
         return view('Clone-gram.complete_user');
     }
 
     //マイページ
-    public function my_page(){
+    public function my_page()
+    {
         return view('Clone-gram.my_page');
     }
 
     //プロフィール編集画面
-    public function edit_profile(){
+    public function edit_profile()
+    {
         return view('Clone-gram.edit_profile');
     }
 
     //プロフィール編集確認画面
-    public function confirm_profile(){
+    public function confirm_profile()
+    {
         return view('Clone-gram.confirm_profile');
     }
 
 
     //他ユーザーのページ
-    public function friends_page(){
+    public function friends_page()
+    {
         return view('Clone-gram.friends_page');
     }
 
     //フォロワー表示画面
-    public function follower(){
+    public function follower()
+    {
         return view('Clone-gram.follower');
     }
 
     //フォロ中のユーザーの表示画面
-    public function following(){
+    public function following()
+    {
         return view('Clone-gram.following');
     }
 
     //チャット画面表示
-    public function chat(){
+    public function chat()
+    {
         return view('Clone-gram.chat');
     }
 
+    //画像投稿
+    public function add_article()
+    {
+        return view('Clone-gram.add_article');
+    }
 
+    /* POST 送信された画像ファイルを受け取って、storageに保存する */
+    public function upload(Request $request)
+    {
+        /* バリデーション */
+        $request->validate([
+            'image' => 'required|max:1024|mimes:jpg,jpeg,png,gif'
+        ]);
 
+        /* store('保存先ディレクトリ', 'ディスク') メソッドで、ファイルをstorage/public/images ディレクトリに保存する
+         * ファイル名はランダム文字列になり、store()の返り値として取得できる
+         * 第2引数は、config/filesystems.php で設定したdisks 配列のキーから指定する
+         */
+        $file_path = $request->image->store('images', 'public');
 
+        /* 保存した画像を表示する */
+        print("<img src='" . asset("$file_path") . "' width='300'>");
 
+        print("<a href='add_article'>画像投稿フォームに戻る</a>");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 
 
@@ -43,37 +43,59 @@ Route::get('/Clone-gram/chat', [CloneGramController::class, 'chat']);
 
 
 //ホーム画面（一覧画面）表示
-Route::get('/Clone-gram/index', [CloneGramController::class, 'index'] );
+Route::get('/Clone-gram/index', [CloneGramController::class, 'index']);
 
 
- //投稿ページ
-Route::get('/Clone-gram/post_article', [CloneGramController::class, 'post_article'] );
+//投稿ページ
+Route::get('/Clone-gram/post_article', [CloneGramController::class, 'post_article']);
 
 
 //投稿した記事の編集画面
-Route::get('/Clone-gram/edit_article', [CloneGramController::class, 'edit_article'] );
+Route::get('/Clone-gram/edit_article', [CloneGramController::class, 'edit_article']);
 
 
 //マイページ
-Route::get('/Clone-gram/my_page', [CloneGramController::class, 'my_page'] );
+Route::get('/Clone-gram/my_page', [CloneGramController::class, 'my_page']);
 
 
 //プロフィール編集画面
-Route::get('/Clone-gram/edit_profile', [CloneGramController::class, 'edit_profile'] );
+Route::get('/Clone-gram/edit_profile', [CloneGramController::class, 'edit_profile']);
 
 
 //プロフィール編集確認画面
-Route::get('/Clone-gram/confirm_profile', [CloneGramController::class, 'confirm_profile'] );
+Route::get('/Clone-gram/confirm_profile', [CloneGramController::class, 'confirm_profile']);
 
 
 //他ユーザーのページ
-Route::get('/Clone-gram/friends_page', [CloneGramController::class, 'friends_page'] );
+Route::get('/Clone-gram/friends_page', [CloneGramController::class, 'friends_page']);
 
 
 //フォロワー表示画面
-Route::get('/Clone-gram/follower', [CloneGramController::class, 'follower'] );
+Route::get('/Clone-gram/follower', [CloneGramController::class, 'follower']);
 
 
 //フォロ中のユーザーの表示画面
-Route::get('/Clone-gram/following', [CloneGramController::class, 'following'] );
+Route::get('/Clone-gram/following', [CloneGramController::class, 'following']);
 
+/* 画像アップロードフォームを表示するルーティング */
+Route::get('/Clone-gram/add_article', [CloneGramController::class, 'add_article']);
+
+/* POST 送信された画像を受け取って保存するルーティング */
+Route::post('/Clone-gram/add_article', [CloneGramController::class, 'upload']);
+
+// /* Storage ファサードを使ってファイルの操作をしてみる */
+// Route::get('storage_test', function () {
+//     /* タイムスタンプを含めたテキストファイル名を作成 */
+//     $filename = time() . '.txt';
+//     /* テキストファイルの内容を作成 */
+//     $content = "ファイル名: {$filename}";
+
+//     /* Storage::put(<ファイルパス>, <内容>) で、ファイルを作成
+//      * ファイル名だけ記載した場合は、操作対象のdisk の直下に作成される
+//      */
+//     Storage::put($filename, $content);
+
+//     /* Storage::files(ファイルパス) で、ファイルの一覧を取得 */
+//     $files = Storage::files();
+//     dump($files);
+// });
