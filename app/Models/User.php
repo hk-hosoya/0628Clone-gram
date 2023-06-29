9ca77cb738cb51571efa2efd70fd441c7fae09a2
USENGROUP\hk-hosoya <hk-hosoya@usen-next.jp>
2023-06-29 15:13:42 +0900

    followerstable

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,14 +11,15 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
+        'screen_name',
         'name',
+        'profile_image',
         'email',
         'password',
     ];
